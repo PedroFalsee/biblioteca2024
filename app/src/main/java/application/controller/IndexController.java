4aeee7697ea5fa9c7f773eb0cf4b842701a745b1
PedroFalsee 123 <esphfm123@gmail.com>
2024-09-09 17:48:21 +0000

    biblioteca-2024

--- app/src/main/java/application/controller/IndexController.java
+++ app/src/main/java/application/controller/IndexController.java
@@ -1,5 +1,4 @@
 package application.controller;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
     @RequestMapping("/")
     public String home() {
+        // return "WEB-INF/home.jsp"; return "home";
         return "home";
     }
 }
-
